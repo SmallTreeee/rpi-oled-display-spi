bbd80c5bb123790fa7dde08552f44ad592a384a5
kszere <witryna.k@gmail.com>
2015-01-28 21:45:44 +0100

    Update OLEDtext.py

--- python-examples/OLEDtext.py
+++ python-examples/OLEDtext.py
@@ -1,28 +1,17 @@
-# OLEDtext.py
-# This program is the simplest in the whole repo. All it does is prints 3 'Hello!'s in various forms on the OLED display.
-# It illustrates how to change the font size and positioning of text on the OLED... As well as showing how to do 
-# basic text!
-# Imports the necessary libraries... Gaugette 'talks' to the display ;-)
+#IMPORT POTRZEBNYCH BIBLIOTEK
 import gaugette.ssd1306
 import time
 import sys
 
-# Setting some variables for our reset pin etc.
-RESET_PIN = 15
-DC_PIN    = 16
+#USTAWIENIE ZMIENNYCH
+RESET_PIN = 5 #(GPIO 24)
+DC_PIN    = 4 #(GPIO 23)
 
-# Very important... This lets py-gaugette 'know' what pins to use in order to reset the display
 led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
 led.begin()
-led.clear_display() # This clears the display but only when there is a led.display() as well!
+led.clear_display()
 
-# led.draw_text2(x-axis, y-axis, whatyouwanttoprint, size) < Understand?
-# So led.drawtext2() prints simple text to the OLED display like so:
-
-text = 'Hello!'
-led.draw_text2(0,0,text,2)
-text2 = 'Hello!'
-led.draw_text2(0,16,text2,1)
-text3 = 'Hello!'
-led.draw_text2(32,25,text3,1)
+led.draw_text2(0,0,"KRPi!",2)
+led.draw_text2(0,16,"KRPi!",1)
+led.draw_text2(32,25,"KRPi!",1)
 led.display()
